da5b7b7027b830d11a8dcdb4ace205144567abf7
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-13 13:05:57 +0530

    [Gyanendra] Add: A Query to Add Table in Database

--- payroll_service/Services.sql
+++ payroll_service/Services.sql
@@ -1,1 +1,5 @@
 create database payroll_services;
+use payroll_services;
+
+create table employee_payroll(id int identity (1,1) primary key, Name varchar(20), Salary float, StartDate date);
+select * from employee_payroll
