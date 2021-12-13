94e446b16c33437b40f6292de756436b5b040060
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-13 17:36:21 +0530

    [Gyanendra] Add: A Query to Add Gender of Employee and Update Rows

--- payroll_service/Services.sql
+++ payroll_service/Services.sql
@@ -15,3 +15,11 @@ select * from employee_payroll
 select Salary from employee_payroll WHERE name  = 'Gyanendra'
 select * from employee_payroll
 where StartDate Between CAST ('2021-12-13' AS Date) and GETDATE();
+
+Alter table employee_payroll Add gender char(1);
+UPDATE employee_payroll Set gender ='M' where name = 'Gyanendra' ;
+UPDATE employee_payroll Set gender ='M' where name = 'Mohan' ;
+UPDATE employee_payroll Set gender ='M' where name = 'Sarika' ;
+UPDATE employee_payroll Set gender ='M' where name = 'Rohan' ;
+
+
